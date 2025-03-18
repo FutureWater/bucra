870c4241f96442910f8e5ccc57af6bd961bce3a9
thomasve-fw <t.vaneldik@futurewater.nl>
2025-03-18 15:51:50 +0100

    Working RET resample code. Not yet tested, missing input data.

--- Croptimal/03_Python_Scripts/01d_Resample_files_Ref_ET.py
+++ Croptimal/03_Python_Scripts/01d_Resample_files_Ref_ET.py
@@ -21,17 +21,27 @@ This script processes reference evapotranspiration (ET) data by:
 ####################################################################################################
 ########################## Define directories and file paths #######################################
 ####################################################################################################
-# Define directories and file paths (constants in UPPERCASE)
-DATA_DIR = "your_data_directory"
-RESULTS_DIR = "your_results_directory"
-PROVINCE_NAME = "your_province_name"
+# Define directories and file paths
+current_wd = os.getcwd()
+parent_wd = os.path.dirname(current_wd)
+angola_wd = "/Users/thomasfuturewater/FutureWater Dropbox/Team/Projects/Completed/2019/2019019_G4AW_MavoDiami_Angola/Data/2019019_MavoDiami_LV/2019019_MavoDiami"
+
+# Gets province from subprocess in 000_Run_All.py
+PROVINCE_NAME = os.environ.get("PROVINCE")
+PROVINCE_NAME = "Zaire"                         # dummy variable for testing.
+
+# Set directories
+DATA_DIR = os.path.join(angola_wd, "01_Data")
+GIS_DIR = os.path.join(angola_wd, "02_GIS")     # Directory with shapefiles
+RESULTS_DIR = os.path.join(parent_wd, "04_Results", PROVINCE_NAME)
+TEMP_DIR = os.path.join(parent_wd, "05_Temp")
 RES = 250  # Set resolution in meters
 LOCAL_PROJ = "EPSG:32733"
 
 # Define input and output paths
 INPUT_FILES = glob.glob(os.path.join(DATA_DIR, "Ref_ET/*.ymonmean.nc"))
-NEW_DIR = os.path.join(RESULTS_DIR, PROVINCE_NAME, "Ref_ET/Mean_Monthly/")
-os.makedirs(NEW_DIR, exist_ok=True)
+REF_ET_RESULTS_DIR = os.path.join(RESULTS_DIR, "Ref_ET/Mean_Monthly/")
+os.makedirs(REF_ET_RESULTS_DIR, exist_ok=True)
 
 ####################################################################################################
 ################################# Read and process NetCDF data ######################################
@@ -39,7 +49,7 @@ os.makedirs(NEW_DIR, exist_ok=True)
 # Multiplier for days in each month
 MULTIPLIER = np.array([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
 
-# Open NetCDF file
+# Open NetCDF file to get time information
 with nc.Dataset(INPUT_FILES[0], "r") as dataset:
     time_nc = dataset.variables["time"][:]
     st_date = nc.num2date(time_nc, dataset.variables["time"].units)
@@ -56,11 +66,12 @@ data_mean = np.mean(np.stack(brick_data), axis=0)
 
 # Crop raster to extent of the province
 print(f"Cropping ET raster for {PROVINCE_NAME}")
-province_shp_path = os.path.join(
-    RESULTS_DIR, PROVINCE_NAME, "Shapefiles", f"{PROVINCE_NAME}.shp")
-province_shp = gpd.read_file(province_shp_path)
-province_shp_proj = province_shp.to_crs(LOCAL_PROJ)
+PROVINCES_FILEPATH = os.path.join(GIS_DIR, "Shapefiles", "AGO_adm1.shp")
+provinces_shp = gpd.read_file(PROVINCES_FILEPATH)
+PROVINCE_SHP_SEL = provinces_shp[provinces_shp["NAME_1"] == PROVINCE_NAME]
+province_shp_proj = PROVINCE_SHP_SEL.to_crs(LOCAL_PROJ)
 
+# Crop raster to province boundary and get transform, extent, and metadata.
 with rasterio.open(INPUT_FILES[0]) as src:
     out_image, out_transform = mask(src, province_shp_proj.geometry, crop=True)
     out_meta = src.meta.copy()
@@ -75,9 +86,9 @@ with rasterio.open(INPUT_FILES[0]) as src:
 ####################################################################################################
 ################################# Resample ET raster to match DEM ##################################
 ####################################################################################################
-# Read resampled DEM raster
+# Read resampled DEM raster and get profile
 dem_diff_path = os.path.join(
-    RESULTS_DIR, PROVINCE_NAME, "DEM", f"DEM_{PROVINCE_NAME}_{RES}m_diff.tif")
+    RESULTS_DIR, "DEM", f"DEM_{PROVINCE_NAME}_{RES}m.tif")  # Used to be: "DEM_{PROVINCE_NAME}_{RES}m_diff.tif"
 with rasterio.open(dem_diff_path) as dem_src:
     dem_profile = dem_src.profile
 
@@ -95,12 +106,11 @@ reproject(
 )
 
 # Apply multiplier and write final ET rasters
-print(f"Writing final ET rasters for {PROVINCE_NAME}")
-MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+print(f"Writing final ET raster: {REF_ET_RESULTS_DIR, PROVINCE_NAME}")
+MONTH_ABBRS = [calendar.month_abbr[i] for i in range(1, 13)]
 
-for i, month in enumerate(MONTHS):
-    output_path = os.path.join(NEW_DIR, f"{month}.tif")
+for i, month in enumerate(MONTH_ABBRS):
+    output_path = os.path.join(REF_ET_RESULTS_DIR, f"{month}.tif")
     with rasterio.open(output_path, "w", **dem_profile) as dst:
         dst.write(resampled_raster * MULTIPLIER[i], 1)
 
